1208d4ab17f82d7f9566745f05d8a672a3215794
Jay <jpincay93@outlook.com>
2025-12-16 17:43:25 -0500

    working on header cta

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const Header = () =>
 {
+    const [open, setOpen] = useState(false);
+
+    const handleOpenMenu = () =>
+    {
+        setOpen((isOpen) => !isOpen);
+    }
+
     return (
         <header>
             <img className='logo' src="./public/qts-logo.svg" alt="quick-tech-solutions" />
+            <button onClick={handleOpenMenu} aria-controls='navBar' className='toggle-menu'>
+                <span class="material-symbols-outlined">
+                    menu
+                </span>
+            </button>
+            <nav className='menu' id='navBar'>
 
-            <nav className='menu'>
-                <ul>
+                <ul className={`menu-list ${open ? "open" : ""}`}>
                     <li>Services</li>
                     <li>Why us</li>
                     <li>Contact</li>
                 </ul>
-                <button aria-labelledby='Menu Toggle Button' className='toggle-menu'>
-                    <span class="material-symbols-outlined">
-                        menu
-                    </span>
-                </button>
             </nav>
-            <div className='cta'>
+            <button className='cta'>
                 CTA
-            </div>
+            </button>
         </header>
     )
 }
