1974ffbfe9b6eaf9bcfe077d2917e7711f855d16
Jasson <jpincay93@outlook.com>
2025-12-15 23:33:41 -0500

    working on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,18 @@ export const Header = () =>
     return (
         <header>
             <img className='logo' src="./public/qts-logo.svg" alt="quick-tech-solutions" />
+
             <nav className='menu'>
                 <ul>
                     <li>Services</li>
                     <li>Why us</li>
                     <li>Contact</li>
                 </ul>
+                <button aria-labelledby='Menu Toggle Button' className='toggle-menu'>
+                    <span class="material-symbols-outlined">
+                        menu
+                    </span>
+                </button>
             </nav>
             <div className='cta'>
                 CTA
