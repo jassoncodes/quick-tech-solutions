9cce44d4cd24df75669efa734b96486fd62c474b
Jay <jpincay93@outlook.com>
2025-12-19 17:30:02 -0500

    working con contact form updated

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -10,12 +10,21 @@ export const ContactForm = () =>
     return (
         <section className='contact-form-section'>
             <form className='contact-form' onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("firstName")} />
-                <select {...register("gender")}>
-                    <option value="female">female</option>
-                    <option value="male">male</option>
-                    <option value="other">other</option>
+                <input placeholder='Name' {...register("firstName")} />
+                <input placeholder='Last Name' {...register("lastName")} />
+                <input placeholder='Phone' {...register("phone")} />
+                <input placeholder='Email' {...register("email")} />
+                <select {...register("service")}>
+                    <option value="">Please select the service your interested in...</option>
+                    <option value="computer-diagnostic">Computer Check-Up & Diagnostics</option>
+                    <option value="network-setup">Wi-Fi Setup & Coverage Optimization</option>
+                    <option value="printer-setup">Printer Installation</option>
+                    <option value="backup-setup">Important File Backup</option>
+                    <option value="remote-support">Fast Remote Support</option>
+                    <option value="onsite-support">Fast Remote Support</option>
+                    <option value="tech-training">Technology Training</option>
                 </select>
+                <textarea placeholder='Can you provide more details?' {...register("email")} />
                 <input type="submit" />
             </form>
         </section>
